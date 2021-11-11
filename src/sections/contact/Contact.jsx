409323a2bea59b4a1611112d29b10f30ce349d6b
Vidar Magnusson <git@vidarmagnusson.com>
2021-11-11 13:28:18 +0100

    Formatting and slight programming language fix

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import data from "../../data/cv.json";
 import {
-    ContactCard, ContactCardsContainer,
+    ContactCard, ContactCardInner, ContactCardsContainer,
     ContactContainer,
     ContactEntry,
     ContactText, Left,
@@ -25,92 +25,95 @@ const Contact = () => (
         </TitleText>
         <ContactCardsContainer>
             <ContactCard>
-                <CenteredTitleText>
-                    Contact
-                </CenteredTitleText>
-                <WideLine/>
-                <ContactEntry>
-                    <StyledIconButton href={"mailto: " + data.cv.contactData.email}>
-                        <MailOutlineIcon fontSize="large"/>
-                    </StyledIconButton>
-                    <ContactText style={{marginLeft: "10px"}}>
-                        {data.cv.contactData.email}
-                    </ContactText>
-                </ContactEntry>
-                <ContactEntry>
-                    <StyledIconButton href={data.cv.contactData.linkedIn.full}>
-                        <LinkedIn fontSize="large" style={{color: "#0077bf"}}/>
-                    </StyledIconButton>
-                    <ContactText>
-                        {data.cv.contactData.linkedIn.short}
-                    </ContactText>
-                </ContactEntry>
-                <ContactEntry>
-                    <StyledIconButton href={data.cv.contactData.github.full}>
-                        <GitHub fontSize="large" style={{color: "#24292e"}}/>
-                    </StyledIconButton>
-                    <ContactText>
-                        {data.cv.contactData.github.short}
-                    </ContactText>
-                </ContactEntry>
+                <ContactCardInner>
+                    <CenteredTitleText>
+                        Contact
+                    </CenteredTitleText>
+                    <WideLine/>
+                    <ContactEntry>
+                        <StyledIconButton href={"mailto: " + data.cv.contactData.email}>
+                            <MailOutlineIcon fontSize="large"/>
+                        </StyledIconButton>
+                        <ContactText style={{marginLeft: "10px"}}>
+                            {data.cv.contactData.email}
+                        </ContactText>
+                    </ContactEntry>
+                    <ContactEntry>
+                        <StyledIconButton href={data.cv.contactData.linkedIn.full}>
+                            <LinkedIn fontSize="large" style={{color: "#0077bf"}}/>
+                        </StyledIconButton>
+                        <ContactText>
+                            {data.cv.contactData.linkedIn.short}
+                        </ContactText>
+                    </ContactEntry>
+                    <ContactEntry>
+                        <StyledIconButton href={data.cv.contactData.github.full}>
+                            <GitHub fontSize="large" style={{color: "#24292e"}}/>
+                        </StyledIconButton>
+                        <ContactText>
+                            {data.cv.contactData.github.short}
+                        </ContactText>
+                    </ContactEntry>
+                </ContactCardInner>
             </ContactCard>
             <ContactCard>
-                <CenteredTitleText>
-                    About me
-                </CenteredTitleText>
-                <WideLine />
-                <Left>
-                    <Typography>
-                        <Box fontWeight={700} fontSize={20}>
-                            My top traits
-                        </Box>
-                    </Typography>
-                </Left>
-                <DutiesList>
-                    {
-                        data.cv.skills.general.map(skill => (
-                        <li>
-                            {skill}
-                        </li>
-                        ))
-                    }
-                </DutiesList>
-                <WideLine />
-                <Left>
-                    <Typography>
-                        <Box fontWeight={700} fontSize={20}>
-                            My top programming langauges
-                        </Box>
-                    </Typography>
-                </Left>
-                <DutiesList>
-                    {
-                        data.cv.skills.programmingLanguages.map(lang => (
-                        <li>
-                            {lang}
-                        </li>
-                        ))
-                    }
-                </DutiesList>
-                <WideLine />
-                <Left>
-                    <Typography>
-                        <Box fontWeight={700} fontSize={20}>
-                            Fluent languages
-                        </Box>
-                    </Typography>
-                </Left>
-                <DutiesList>
-                    {
-                        data.cv.skills.languages.map(lang => (
-                        <li>
-                            {lang}
-                        </li>
-                        ))
-                    }
-                </DutiesList>
-
-            </ContactCard>
+                <ContactCardInner>
+                    <CenteredTitleText>
+                        About me
+                    </CenteredTitleText>
+                    <WideLine />
+                    <Left>
+                        <Typography>
+                            <Box fontWeight={700} fontSize={20}>
+                                My top traits
+                            </Box>
+                        </Typography>
+                    </Left>
+                    <DutiesList>
+                        {
+                            data.cv.skills.general.map(skill => (
+                            <li>
+                                {skill}
+                            </li>
+                            ))
+                        }
+                    </DutiesList>
+                    <WideLine />
+                    <Left>
+                        <Typography>
+                            <Box fontWeight={700} fontSize={20}>
+                                My top programming langauges
+                            </Box>
+                        </Typography>
+                    </Left>
+                    <DutiesList>
+                        {
+                            data.cv.skills.programmingLanguages.map(lang => (
+                            <li>
+                                {lang}
+                            </li>
+                            ))
+                        }
+                    </DutiesList>
+                    <WideLine />
+                    <Left>
+                        <Typography>
+                            <Box fontWeight={700} fontSize={20}>
+                                Fluent languages
+                            </Box>
+                        </Typography>
+                    </Left>
+                    <DutiesList>
+                        {
+                            data.cv.skills.languages.map(lang => (
+                            <li>
+                                {lang}
+                            </li>
+                            ))
+                        }
+                    </DutiesList>
+                </ContactCardInner>
+                </ContactCard>
         </ContactCardsContainer>
     </ContactContainer>
 )
